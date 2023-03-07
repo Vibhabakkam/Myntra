7c570e3ff066efeea47fc4a05be3dfc60c727474
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-07 19:55:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ function register(event) {
     var userPhone = document.getElementById("userPhone").value;
     var userEmail = document.getElementById("userEmail").value;
     var userPassword = document.getElementById("userPassword").value;
-    var userData = { name: userName, number : userPhone, email: userEmail, password: userPassword }
+    var MyntrauserData = { name: userName, number : userPhone, email: userEmail, password: userPassword }
 
     // storing data from js to ls
-    var dataFromLS = JSON.parse(localStorage.getItem("userData")) || [];
+    var dataFromLS = JSON.parse(localStorage.getItem("MyntrauserData")) || [];
     console.log(dataFromLS, 'dataFromLS')
     var flag = false;
     for (var i = 0; i < dataFromLS.length; i++) {
@@ -23,8 +23,8 @@ function register(event) {
     } else if (userPassword.length < 8) {
         alert("password must be more than 8 digit")
     } else {
-        dataFromLS.push(userData);
-        localStorage.setItem("userData", JSON.stringify(dataFromLS));
+        dataFromLS.push(MyntrauserData);
+        localStorage.setItem("MyntrauserData", JSON.stringify(dataFromLS));
         document.getElementById("userName").value = " ";
         document.getElementById("userPhone").value = " ";
         document.getElementById("userEmail").value = " ";
@@ -39,25 +39,97 @@ function login(event){
     event.preventDefault();
     var userEmail = document.getElementById("email").value;
     var userPassword = document.getElementById("password").value;
-    var userData = {email : userEmail, password : userPassword}
+    var MyntrauserData = { email: userEmail, password: userPassword }
 
-    var dataFromLS = JSON.parse(localStorage.getItem("userData")) || [];
+    var dataFromLS = JSON.parse(localStorage.getItem("MyntrauserData")) || [];
     console.log(dataFromLS, 'dataFromLS')
     var flag = false;
-    for(var i = 0; i < dataFromLS.length; i++){
-        if(dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword){
+    for (var i = 0; i < dataFromLS.length; i++) {
+        if (dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword) {
             flag = true;
         }
     }
-    if(flag === true){
-        dataFromLS.push(userData);
-        localStorage.setItem("userData",JSON.stringify(dataFromLS));
-        document.getElementById("email").value =" ";
-        document.getElementById("password").value = " ";
-        window.location.href = '/Myntra.html';
+    if (flag === true) {
         alert("login succecfully")
+        dataFromLS.push(MyntrauserData);
+        localStorage.setItem("MyntrauserData", JSON.stringify(dataFromLS));
+        document.getElementById("email").value = " ";
+        document.getElementById("password").value = " ";
+        var User = {};
+        User = { email: userEmail }
+        localStorage.setItem("currentUser", JSON.stringify(User));
+
+
+        window.location.href = '/Myntra.html';
     } else {
-        
+
         alert("Wrong cred, Please check your email and password");
     }
+}
+
+var gettingEmail;
+function forgetPassword() {
+
+    var dataFromLS = JSON.parse(localStorage.getItem("MyntrauserData"));
+    var userEmail = document.getElementById("email").value;
+    gettingEmail = userEmail;
+
+    var flag = false;
+    for (var i = 0; i < dataFromLS.length; i++) {
+        if (dataFromLS[i].email === userEmail) {
+            flag = true;
+        }
+    }
+    if (flag === true) {
+        // window.location.href = '/newpass.html';
+
+        var newCode = `<input type = "password" id ="password" > <br> <button id="regdiv" onclick="newPassword()">set new password</button>`
+        var divFromHtml = document.getElementById("change")
+        divFromHtml.innerHTML = newCode;
+        alert(" now set new password")
+    }
+    else {
+        alert(" email not found ,check email again ")
+    }
+
+
+}
+
+
+function newPassword() {
+    var userPassword = document.getElementById("password").value;
+    var dataFromLS = JSON.parse(localStorage.getItem("MyntrauserData"));
+    //    alert ("worked")
+
+
+    for (var i = 0; i < dataFromLS.length; i++) {
+        if (dataFromLS[i].email === gettingEmail) {
+            dataFromLS[i].password = userPassword
+        }
+    }
+    localStorage.setItem('MyntrauserData', JSON.stringify(dataFromLS));
+    gettingEmail = "";
+
+    window.location.href = '/myntralogin.html';
+    alert("password change now login")
+
+}
+function addToLS() {
+    alert("product added")
+    var Name = document.getElementById("proName").value;
+    var Imge = document.getElementById("proImg").value;
+    var Price = document.getElementById("proPrice").value;
+
+    var MyntrauserProduct = {  proImg:Imge, proName: Name, proPrice:Price }
+    // store deta from java script to local storage
+    var dataFromLS = JSON.parse(localStorage.getItem("MyntrauserProduct")) || [];
+    console.log(dataFromLS, 'dataFromLS')
+  
+    dataFromLS.push(MyntrauserProduct);
+    localStorage.setItem("MyntrauserProduct", JSON.stringify(dataFromLS));
+    alert("product added")
+
+    var Name = document.getElementById("proName").value ="" ;
+    var Imge = document.getElementById("proImg").value ="";
+    var Price = document.getElementById("proPrice").value ="";
 }
